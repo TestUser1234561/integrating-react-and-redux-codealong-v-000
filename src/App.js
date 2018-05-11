57687bb89216d3c867effaae9038ecf0c3744dd7
Morgan Fudge <mfudge92@gmail.com>
2018-05-11 18:52:20 +0000

    automatically backed up by learn

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Counter from './components/Counter';
- 
+
 class App extends Component {
   render() {
     return (
@@ -10,5 +10,5 @@ class App extends Component {
     );
   };
 };
- 
+
 export default App;
